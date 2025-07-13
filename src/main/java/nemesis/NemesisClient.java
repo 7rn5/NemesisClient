ee7cb59225dc67ab632101e77b6e593f13592c88
7rn5 <zyagaimo7103@gmail.com>
2025-07-13 20:33:44 +0900

    Add files via upload

---
+++ src/main/java/nemesis/NemesisClient.java
@@ -0,0 +1,30 @@
+package nemesis;
+
+import nemesis.manager.misc.*;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NemesisClient implements ModInitializer {
+	public static final String CLIENT_ID = "nemesis";
+  public static final String CLIENT_VERSION = "0.0.0";
+  public static final String CLIENT_NAME = "NemesisClient";
+	
+	public static MinecraftClient mc;
+	
+	//managers
+	public static ConfigManager configManager;
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(CLIENT_ID);
+
+	@Override
+	public void onInitialize() {
+	    //log
+	    LOGGER.info("Welcome to NemesisClient");
+	    
+	    //nemesis managers
+	    configManager = new ConfigManager();
+	}
+}
