d55d95102a8de3932210a3baff44b4cc2ffccb55
7rn5 <zyagaimo7103@gmail.com>
2025-07-15 16:38:10 +0900

    update src/main/java/nemesis/manager/misc/ConfigManager.java

--- src/main/java/nemesis/manager/misc/ConfigManager.java
+++ src/main/java/nemesis/manager/misc/ConfigManager.java
@@ -1,16 +1,42 @@
 package nemesis.manager.misc;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import nemesis.NemesisClient;
+import com.google.gson.*;
+import nemesis.util.file.JsonUtil;
 import net.fabricmc.loader.api.FabricLoader;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class ConfigManager {
-    private static final Path NEMESIS_PATH = FabricLoader.getInstance().getGameDir().resolve("nemesis");
+    private final List<JsonUtil> jsonables = List.of();
+    
+    private static final Path NEMESIS_PATH = FabricLoader.getInstance().getGameDir().resolve(NemesisClient.CLIENT_ID);
     private static final Gson gson = new GsonBuilder()
             .setPrettyPrinting()
             .create();
     
-    //public void save() {}
-    //public void load() {}
+    public void load() {
+        if (!NEMESIS_PATH.toFile().exists()) NEMESIS_PATH.toFile().mkdirs();
+        for (JsonUtil jsonable : jsonables) {
+            try {
+                String read = Files.readString(NEMESIS_PATH.resolve(jsonable.getFileName()));
+                jsonable.fromJson(JsonParser.parseString(read));
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void save() {
+        if (!NEMESIS_PATH.toFile().exists()) NEMESIS_PATH.toFile().mkdirs();
+        for (JsonUtil jsonable : jsonables) {
+            try {
+                JsonElement json = jsonable.toJson();
+                Files.writeString(NEMESIS_PATH.resolve(jsonable.getFileName()), gson.toJson(json));
+            } catch (Throwable e) {
+            }
+        }
+    }
 }
