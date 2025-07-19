4bd6079765cfb280a1894e00f91c7f843035c693
7rn5 <zyagaimo7103@gmail.com>
2025-07-19 11:35:10 +0900

    update src/main/java/nemesis/mixin/SplashTextResourceSupplierMixin.java

--- src/main/java/nemesis/mixin/SplashTextResourceSupplierMixin.java
+++ src/main/java/nemesis/mixin/SplashTextResourceSupplierMixin.java
@@ -1,9 +1,10 @@
-/*package nemesis.mixin;
+package nemesis.mixin;
 
 import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.resource.SplashTextResourceSupplier;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -13,16 +14,25 @@ import java.util.Random;
 
 @Mixin(SplashTextResourceSupplier.class)
 public abstract class SplashTextResourceSupplierMixin {
+    @Unique
+    private static final Random random = new Random();
+    
+    @Unique
+    private final List<String> splashes = getSplashes();
+
     @Inject(method = "get", at = @At("HEAD"), cancellable = true)
-    private void injectSplash(CallbackInfoReturnable<String> cir) {
-        List<String> list = List.of(
-            "NemesisClient",
-            "Welcome to Nemesis",
-            "Best client",
-            "Nemesis for 2b2t",
-            "2b2t.org"
-        );
-        String splash = list.get(new Random().nextInt(list.size()));
-        cir.setReturnValue(splash);
+    private void onApply(CallbackInfoReturnable<SplashTextRenderer> cir) {
+        cir.setReturnValue(new SplashTextRenderer(splashes.get(random.nextInt(splashes.size()))));
     }
-}*/
+    
+    @Unique
+    private static List<String> getSplashes() {
+        return List.of(
+              "NemesisClient",
+              "Welcome to Nemesis",
+              "Best Client",
+              "Nemesis for 2b2t",
+              "2b2t.org"
+        );
+    }
+}
