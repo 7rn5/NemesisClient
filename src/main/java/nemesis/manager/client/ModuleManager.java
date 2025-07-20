53bc794a7bfa9f3817dfc56f546bb63168b8c325
7rn5 <zyagaimo7103@gmail.com>
2025-07-21 00:52:25 +0900

    update src/main/java/nemesis/manager/client/ModuleManager.java

--- src/main/java/nemesis/manager/client/ModuleManager.java
+++ src/main/java/nemesis/manager/client/ModuleManager.java
@@ -1,6 +1,10 @@
 package nemesis.manager.client;
 
+import java.util.ArrayList;
+
 public class ModuleManager {
+    public final ArrayList<Module> modules = new ArrayList<>();
+    
     public ModuleManager() {
     }
 }
