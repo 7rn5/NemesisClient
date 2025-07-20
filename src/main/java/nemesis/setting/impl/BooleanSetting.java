1fb210274b115873a59c01bb2652b93fd4dffaab
7rn5 <zyagaimo7103@gmail.com>
2025-07-21 00:53:38 +0900

    Updated 7/21

--- src/main/java/nemesis/setting/impl/BooleanSetting.java
+++ src/main/java/nemesis/setting/impl/BooleanSetting.java
@@ -1,13 +1,41 @@
 package nemesis.setting.impl;
 
+import nemesis.manager.client.ModuleManager;
 import nemesis.setting.Setting;
 
-public class BooleanSetting extends Setting<Boolean> {
+public class BooleanSetting extends Setting {
+    public Runnable task = null;
+    public boolean injectTask = false;
+    private boolean value;
+    public final boolean defaultValue;
+    
     public BooleanSetting(String name, boolean defaultValue) {
-        super(name, defaultValue);
+        super(name);
+        this.defaultValue = defaultValue;
+        this.value = defaultValue;
     }
+    
+    public final boolean get() { //getValueよりgetのほうが短いからgetにしてる
+		    return this.value;
+	  }
+	  
+	  public final void set(boolean value) {
+		    if (injectTask && value != this.value) {
+			      task.run();
+		    }
+		    this.value = value;
+	  }
 
-    public void toggle() {
-        setValue(!getValue());
-    }
+	  public final void toggle() {
+		    set(!value);
+	  }
+	  
+	  public BooleanSetting injectTask(Runnable task) {
+		    this.task = task;
+		    injectTask = true;
+		    return this;
+	  }
 }
+
+// example
+// public final BooleanSetting exampleBoolean = this.add(new BooleanSetting("ExampleBoolean", true));
