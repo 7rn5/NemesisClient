424b80373bf190d91dde24fe5043e8d46ccd681a
7rn5 <zyagaimo7103@gmail.com>
2025-07-21 00:52:40 +0900

    update src/main/java/nemesis/NemesisClient.java

--- src/main/java/nemesis/NemesisClient.java
+++ src/main/java/nemesis/NemesisClient.java
@@ -1,6 +1,7 @@
 package nemesis;
 
 import nemesis.manager.misc.*;
+import nemesis.manager.client.*;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.api.ClientModInitializer;
@@ -10,13 +11,15 @@ import org.slf4j.LoggerFactory;
 
 public class NemesisClient implements ModInitializer {
 	public static final String CLIENT_ID = "nemesis";
-  public static final String CLIENT_VERSION = "0.0.0";
+	public static final String CLIENT_STATUS = "Beta"; //Beta or Release
+  public static final String CLIENT_VERSION = "0.0.1";
   public static final String CLIENT_NAME = "NemesisClient";
 	
 	public static MinecraftClient mc;
 	
 	//managers
 	public static ConfigManager configManager;
+	public static ModuleManager moduleManager;
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(CLIENT_ID);
 
@@ -24,12 +27,14 @@ public class NemesisClient implements ModInitializer {
 	public void onInitialize() {
 	    //log
 	    LOGGER.info("Welcome to NemesisClient");
-	    
-	    //nemesis managers
 	}
 	
 	public void onInitializeClient() {
-	    configManager = new ConfigManager();
+	    mc = MinecraftClient.getInstance();
 	    configManager.load();
+	    
+	    //Register Manager
+	    moduleManager = new ModuleManager();
+	    configManager = new ConfigManager();
 	}
 }
