c5def23ff7665b4665ba99011e920bdf2964e21f
7rn5 <zyagaimo7103@gmail.com>
2025-07-23 22:34:06 +0900

    update ColorSetting.java

---
+++ src/main/java/nemesis/setting/impl/ColorSetting.java
@@ -0,0 +1,16 @@
+//package nemesis.setting.impl;
+
+//import nemesis.setting.Setting;
+
+//import java.util.function.BooleanSupplier;
+
+//public class ColorSetting extends Setting {
+    //private Color value;
+    //private final Color defaultValue;
+    
+    //public ColorSetting(String name, Color defaultValue, BooleanSupplier visible) {
+        //super(name);
+        //this.value = defaultValue;
+        //this.defaultValue = defaultValue;
+    //}
+//}
