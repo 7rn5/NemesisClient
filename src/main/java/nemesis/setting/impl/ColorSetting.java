edac91f318de517a52268ee053c57a538ff0e946
7rn5 <zyagaimo7103@gmail.com>
2025-07-24 02:15:58 +0900

    update src/main/java/nemesis/setting/impl/ColorSetting.java

--- src/main/java/nemesis/setting/impl/ColorSetting.java
+++ src/main/java/nemesis/setting/impl/ColorSetting.java
@@ -1,16 +1,39 @@
-//package nemesis.setting.impl;
+package nemesis.setting.impl;
 
-//import nemesis.setting.Setting;
+import nemesis.setting.Setting;
+import nemesis.util.renderer.ColorUtil;
 
-//import java.util.function.BooleanSupplier;
+import java.awt.Color;
 
-//public class ColorSetting extends Setting {
-    //private Color value;
-    //private final Color defaultValue;
-    
-    //public ColorSetting(String name, Color defaultValue, BooleanSupplier visible) {
-        //super(name);
-        //this.value = defaultValue;
-        //this.defaultValue = defaultValue;
-    //}
-//}
+public class ColorSetting extends Setting<Color> {
+    private Color value;
+    private final Color defaultValue;
+
+    public ColorSetting(String name, Color defaultValue) {
+        super(name);
+        this.value = defaultValue;
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public Color get() {
+        return value;
+    }
+
+    @Override
+    public void set(Color value) {
+        this.value = value;
+    }
+
+    public void reset() {
+        this.value = defaultValue;
+    }
+
+    public Color getDefault() {
+        return defaultValue;
+    }
+
+    public Color getWithAlpha(int alpha) {
+        return ColorUtil.newAlpha(value, alpha);
+    }
+}
