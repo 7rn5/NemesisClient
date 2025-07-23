2ed87cbe8de0302da4b372afd5ddca93be1dd667
7rn5 <zyagaimo7103@gmail.com>
2025-07-24 02:15:23 +0900

    update src/main/java/nemesis/setting/impl/BooleanSetting.java

--- src/main/java/nemesis/setting/impl/BooleanSetting.java
+++ src/main/java/nemesis/setting/impl/BooleanSetting.java
@@ -1,11 +1,11 @@
 package nemesis.setting.impl;
 
-import nemesis.manager.client.ModuleManager;
+//import nemesis.manager.client.ModuleManager;
 import nemesis.setting.Setting;
 
-public class BooleanSetting extends Setting {
-    public Runnable task = null;
-    public boolean injectTask = false;
+public class BooleanSetting extends Setting<Boolean> {
+    //public Runnable task = null;
+    //public boolean injectTask = false;
     private boolean value;
     public final boolean defaultValue;
     
@@ -15,26 +15,40 @@ public class BooleanSetting extends Setting {
         this.value = defaultValue;
     }
     
-    public final boolean get() { //getValueよりgetのほうが短いからgetにしてる
-		    return this.value;
-	  }
+    //@Override
+    public Boolean get() {
+        return value;
+    }
+    
+    public void set(Boolean value) {
+        this.value = value;
+    }
+    
+    public void reset() {
+        this.value = defaultValue;
+    }
+    
+    //@Override
+    //public final boolean get() { //getValueよりgetのほうが短いからgetにしてる
+		//    return this.value;
+	  //}
 	  
-	  public final void set(boolean value) {
-		    if (injectTask && value != this.value) {
-			      task.run();
-		    }
-		    this.value = value;
-	  }
+	  //public final void set(boolean value) {
+		//    if (injectTask && value != this.value) {
+		//	      task.run();
+		//    }
+		//    this.value = value;
+	  //}
 
 	  public final void toggle() {
 		    set(!value);
 	  }
 	  
-	  public BooleanSetting injectTask(Runnable task) {
-		    this.task = task;
-		    injectTask = true;
-		    return this;
-	  }
+	  //public BooleanSetting injectTask(Runnable task) {
+		//    this.task = task;
+		//    injectTask = true;
+		//    return this;
+	  //}
 }
 
 // example
