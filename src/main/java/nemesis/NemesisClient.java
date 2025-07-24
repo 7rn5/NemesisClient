a12603044f2303c38afbb9f1a88380ff6aad41bb
7rn5 <zyagaimo7103@gmail.com>
2025-07-24 12:30:25 +0900

    update src/main/java/nemesis/NemesisClient.java

--- src/main/java/nemesis/NemesisClient.java
+++ src/main/java/nemesis/NemesisClient.java
@@ -1,3 +1,11 @@
+//    ##    ##  ########  ##     ##  ########   ######   ####   ######  
+//    ###   ##  ##        ###   ###  ##        ##    ##   ##   ##    ## 
+//    ####  ##  ##        #### ####  ##        ##         ##   ##       
+//    ## ## ##  ######    ## ### ##  ######     ######    ##    ######  
+//    ##  ####  ##        ##     ##  ##              ##   ##         ## 
+//    ##   ###  ##        ##     ##  ##        ##    ##   ##   ##    ## 
+//    ##    ##  ########  ##     ##  ########   ######   ####   ######  
+
 package nemesis;
 
 import nemesis.manager.misc.*;
@@ -14,19 +22,32 @@ public class NemesisClient implements ModInitializer {
 	public static final String CLIENT_STATUS = "Beta"; //Beta or Release
   public static final String CLIENT_VERSION = "0.0.1";
   public static final String CLIENT_NAME = "NemesisClient";
+  public static final String LOG_PREFIX = "[" + CLIENT_NAME + "]";
 	
 	public static MinecraftClient mc;
 	
 	//managers
 	public static ConfigManager configManager;
 	public static ModuleManager moduleManager;
-
-	public static final Logger LOGGER = LoggerFactory.getLogger(CLIENT_ID);
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(CLIENT_NAME);
 
 	@Override
 	public void onInitialize() {
 	    //log
-	    LOGGER.info("Welcome to NemesisClient");
+	    LOGGER.info(LOG_PREFIX + "Welcome to NemesisClient");
+	    
+	    //Nemesis
+	    LOGGER.info(LOG_PREFIX + "");
+	    LOGGER.info(LOG_PREFIX + " ##    ##  ########  ##     ##  ########   ######   ####   ######  ");
+	    LOGGER.info(LOG_PREFIX + " ###   ##  ##        ###   ###  ##        ##    ##   ##   ##    ## ");
+	    LOGGER.info(LOG_PREFIX + " ####  ##  ##        #### ####  ##        ##         ##   ##       ");
+	    LOGGER.info(LOG_PREFIX + " ## ## ##  ######    ## ### ##  ######     ######    ##    ######  ");
+	    LOGGER.info(LOG_PREFIX + " ##  ####  ##        ##     ##  ##              ##   ##         ## ");
+	    LOGGER.info(LOG_PREFIX + " ##   ###  ##        ##     ##  ##        ##    ##   ##   ##    ## ");
+	    LOGGER.info(LOG_PREFIX + " ##    ##  ########  ##     ##  ########   ######   ####   ######  ");
+	    LOGGER.info(LOG_PREFIX + "");
+	    LOGGER.info(LOG_PREFIX + "    " + CLIENT_NAME + " v" +CLIENT_VERSION + " (" + CLIENT_STATUS + ")");
 	}
 	
 	public void onInitializeClient() {
