b93a5e022dd133281acb1cf8187417549a0e85fb
7rn5 <zyagaimo7103@gmail.com>
2025-07-25 01:02:28 +0900

    update src/main/java/nemesis/setting/impl/ColorSetting.java

--- src/main/java/nemesis/setting/impl/ColorSetting.java
+++ src/main/java/nemesis/setting/impl/ColorSetting.java
@@ -1,6 +1,7 @@
 package nemesis.setting.impl;
 
 import nemesis.setting.Setting;
+import nemesis.setting.impl.GroupSetting;
 import nemesis.util.renderer.ColorUtil;
 
 import java.awt.Color;
@@ -8,6 +9,7 @@ import java.awt.Color;
 public class ColorSetting extends Setting<Color> {
     private Color value;
     private final Color defaultValue;
+    //private final groupName = Setting.getName();
 
     public ColorSetting(String name, Color defaultValue) {
         super(name);
