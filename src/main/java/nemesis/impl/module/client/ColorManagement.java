1def50c0644c189828cd0ee781158fbf4c784c5f
7rn5 <zyagaimo7103@gmail.com>
2025-07-25 01:03:28 +0900

    update src/main/java/nemesis/impl/module/client/ColorManagement.java

--- src/main/java/nemesis/impl/module/client/ColorManagement.java
+++ src/main/java/nemesis/impl/module/client/ColorManagement.java
@@ -3,11 +3,30 @@ package nemesis.impl.module.client;
 import nemesis.impl.module.Module;
 import nemesis.setting.Setting;
 import nemesis.setting.impl.ColorSetting;
+import nemesis.setting.impl.GroupSetting;
 
 import java.awt.Color;
 
 public class ColorManagement extends Module {
-    public final ColorSetting defaultColor = new ColorSetting("DefaultColor", new Color(255, 0, 0));
+    public final GroupSetting general = new GroupSetting("General");
+    
+    public final ColorSetting friendColor = new ColorSetting(
+        "FriendColor",
+        new Color(255, 0, 0)/*,
+        new GroupSetting(general)*/
+    );
+    
+    public final ColorSetting enemyColor = new ColorSetting(
+        "EnemyColor",
+        new Color(255, 0, 0)/*,
+        new GroupSetting(general)*/
+    );
+    
+    public final ColorSetting defaultColor = new ColorSetting(
+        "DefaultColor",
+        new Color(255, 0, 0)/*,
+        new GroupSetting(general)*/
+    );
     
     public ColorManagement() {
         super("ColorManagement", Category.Client, "ColorManager");
