ff50338d1c7c6f72976a20fd43f14779e7de78a7
7rn5 <zyagaimo7103@gmail.com>
2025-07-24 15:59:44 +0900

    update src/main/java/nemesis/impl/Register.java

---
+++ src/main/java/nemesis/impl/Register.java
@@ -0,0 +1,19 @@
+package nemesis.impl;
+
+import nemesis.impl.module.Module;
+import nemesis.impl.module.client.*;
+
+public class Register {
+    public void registerModules() {
+        //Combat
+        //Player
+        //Visual
+        //Movement
+        //Exploit
+        //Client
+        //add(new ColorManagement());
+    }
+    
+    public void registerHud() {}
+    public void registerCommands() {}
+}
