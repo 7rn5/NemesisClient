868b5cd6d881d61b2e68e71d97aaeee975ab0f56
7rn5 <zyagaimo7103@gmail.com>
2025-07-24 16:00:01 +0900

    update src/main/java/nemesis/impl/module/Module.java

--- src/main/java/nemesis/impl/module/Module.java
+++ src/main/java/nemesis/impl/module/Module.java
@@ -1,3 +1,86 @@
 package nemesis.impl.module;
 
-public abstract class Module {}
+import nemesis.setting.Setting;
+//import nemesis.impl.module.Modules;
+//import nemesis.setting.impl.BindSetting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Module {
+    private final String name;
+    private final Category category;
+    private final String description;
+
+    private boolean enabled = false;
+    private final List<Setting<?>> settings = new ArrayList<>();
+    //private static final List<Module> modules = new ArrayList<>();
+
+    //public final BindSetting bind = new BindSetting("Key", -1); 
+
+    public Module(String name, Category category, String description) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+
+        //add(bind); 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+    
+    //public static List<Module> getModules() {
+    //    return Collections.unmodifiableList(modules);
+    //}
+
+    //protected <T extends Setting<?>> T add(T setting) {
+    //    settings.add(setting);
+    //    return setting;
+    //}
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        if (this.enabled != enabled) {
+            this.enabled = enabled;
+            if (enabled) {
+                onEnable();
+            } else {
+                onDisable();
+            }
+        }
+    }
+
+    public void toggle() {
+        setEnabled(!enabled);
+    }
+
+    public void onEnable() {}
+    public void onDisable() {}
+
+    public List<Setting<?>> getSettings() {
+        return settings;
+    }
+
+    protected <T extends Setting<?>> T add(T setting) {
+        settings.add(setting);
+        return setting;
+    }
+    
+    //Category list
+    public enum Category {
+        Combat,
+        Player,
+        Visual,
+        Movement,
+        Exploit,
+        Client
+    }
+}
