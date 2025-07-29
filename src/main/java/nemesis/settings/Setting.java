988419dc113607e8934f6771596af9129c80682a
7rn5 <zyagaimo7103@gmail.com>
2025-07-30 02:13:31 +0900

    Add files via upload

---
+++ src/main/java/nemesis/settings/Setting.java
@@ -0,0 +1,35 @@
+package nemesis.settings;
+
+import com.google.gson.JsonElement;
+
+public abstract class Setting<T> {
+    protected final String name;
+    protected T value;
+    
+    private Runnable onChanged = null;
+    
+    public Setting(String name, T defaultValue) {
+        this.name = name;
+        this.value = defaultValue;
+    }
+    
+    public String getName() {
+         return name;
+    }
+    
+    public T get() {
+        return value;
+    }
+    
+    public void set(T value) {
+        this.value = value;
+        if (onChanged != null) onChanged.run();
+    }
+    
+    public void setOnChanged(Runnable callback) {
+        this.onChanged = callback;
+    }
+    
+    public abstract void fromJson(JsonElement json);
+    public abstract JsonElement toJson();
+}
