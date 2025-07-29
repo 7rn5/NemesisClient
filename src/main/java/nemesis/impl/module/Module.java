2f79d77b30ad359e5e88517edf463297dddde24d
7rn5 <zyagaimo7103@gmail.com>
2025-07-30 02:11:25 +0900

    update src/main/java/nemesis/impl/module/Module.java

--- src/main/java/nemesis/impl/module/Module.java
+++ src/main/java/nemesis/impl/module/Module.java
@@ -1,7 +1,7 @@
-package nemesis.impl.module;
+/*package nemesis.impl.module;
 
-import nemesis.setting.Setting;
-import nemesis.setting.impl.BindSetting;
+import nemesis.settings.Setting;
+//import nemesis.setting.impl.BindSetting;
 //import nemesis.impl.module.Modules;
 //import net.minecraft.client.util.InputUtil;
 //import org.lwjgl.glfw.GLFW;
@@ -14,10 +14,10 @@ public abstract class Module {
     private final Category category;
     private final String description;
     //public final BindSetting bind = new BindSetting("Keybind", new Bind(-1));
-    public final BindSetting bind = new BindSetting("Keybind", -1);
+    //public final BindSetting bind = new BindSetting("Keybind", -1);
 
     private boolean enabled = false;
-    private final List<Setting<?>> settings = new ArrayList<>();
+    //private final List<Setting<?>> settings = new ArrayList<>();
     //private static final List<Module> modules = new ArrayList<>();
 
     //public final BindSetting bind = new BindSetting("Key", -1); 
@@ -87,6 +87,78 @@ public abstract class Module {
     }
     
     //Category list
+    public enum Category {
+        Combat,
+        Player,
+        Visual,
+        Movement,
+        Exploit,
+        Client
+    }
+}
+*/
+
+package nemesis.impl.module;
+
+import nemesis.settings.Setting;
+import nemesis.settings.impl.BindSetting;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Module {
+    private final String name, description;
+    private Category category;
+    private boolean alwaysEnable;
+    private boolean enabled = false;
+    private final BindSetting Bind;
+    
+    private final List<Setting<?>> settings = new ArrayList<>();
+    
+    public Module(String name, String description, Category category, boolean alwaysEnable) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.alwaysEnable = alwaysEnable;
+        
+        this.Bind = new BindSetting("bind", BindSetting.KEY_NONE);
+        this.settings.add(Bind);
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Category getCategory() {
+        return category;
+    }
+    
+    public void setEnabled(boolean enabled) {
+        if (this.enabled != enabled) {
+            this.enabled = enabled;
+            if (enabled) {
+                onEnabled();
+            } else {
+                onDisabled();
+            }
+        }
+    }
+    
+    public boolean isEnabled() {
+        return !enabled;
+    }
+    
+    private void onEnabled() {}
+    private void onDisabled() {}
+    
+    public void toggle() {
+        setEnabled(!enabled);
+    }
+    
+    public List<Setting<?>> getSettings() {
+        return settings;
+    }
+    
     public enum Category {
         Combat,
         Player,
