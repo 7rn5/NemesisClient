023e03e43c42eb6218281f200f2396affdc1e8ba
7rn5 <zyagaimo7103@gmail.com>
2025-07-31 23:14:11 +0900

    Add files via upload

---
+++ src/main/java/nemesis/impl/gui/api/GuiComponent.java
@@ -0,0 +1,9 @@
+package nemesis.impl.gui.api;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.font.TextRenderer;
+
+public interface GuiComponent {
+    void render(DrawContext context, TextRenderer textRenderer, int mouseX, int mouseY, float delta);
+    boolean mouseClicked(double mouseX, double mouseY, int button);
+}
