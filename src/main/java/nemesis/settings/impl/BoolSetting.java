f2735bc1156b3c179783f768b45086a6c45d2074
7rn5 <zyagaimo7103@gmail.com>
2025-08-07 00:16:58 +0900

    update src/main/java/nemesis/settings/impl/BoolSetting.java

--- src/main/java/nemesis/settings/impl/BoolSetting.java
+++ src/main/java/nemesis/settings/impl/BoolSetting.java
@@ -1,7 +1,7 @@
 package nemesis.settings.impl;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
+//import com.google.gson.JsonElement;
+//import com.google.gson.JsonPrimitive;
 import nemesis.settings.Setting;
 
 public class BoolSetting extends Setting<Boolean> {
@@ -13,15 +13,15 @@ public class BoolSetting extends Setting<Boolean> {
         set(!value);
     }
     
-    @Override
-    public void fromJson(JsonElement json) {
-        if (json.isJsonPrimitive() && json.getAsJsonPrimitive().isBoolean()) {
-            this.value = json.getAsBoolean();
-        }
-    }
+    //@Override
+    //public void fromJson(JsonElement json) {
+    //    if (json.isJsonPrimitive() && json.getAsJsonPrimitive().isBoolean()) {
+    //        this.value = json.getAsBoolean();
+    //    }
+    //}
 
-    @Override
-    public JsonElement toJson() {
-        return new JsonPrimitive(value);
-    }
+    //@Override
+    //public JsonElement toJson() {
+    //    return new JsonPrimitive(value);
+    //}
 }
