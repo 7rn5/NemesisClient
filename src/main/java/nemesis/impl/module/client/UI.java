c6d7ae53b793e0120a5d7f6eb79e97411c3b827e
7rn5 <zyagaimo7103@gmail.com>
2025-08-07 00:41:42 +0900

    update src/main/java/nemesis/impl/module/client/UI.java

--- src/main/java/nemesis/impl/module/client/UI.java
+++ src/main/java/nemesis/impl/module/client/UI.java
@@ -3,14 +3,10 @@ package nemesis.impl.module.client;
 import nemesis.impl.module.Module;
 import nemesis.settings.impl.BoolSetting;
 import nemesis.settings.impl.BindSetting;
-import nemesis.event.SubscribeEvent;
-import nemesis.event.EventPriority;
-import nemesis.event.impl.PostTickEvent;
 
 import org.lwjgl.glfw.GLFW;
 
 import static nemesis.NemesisClient.mc;
-import static nemesis.NemesisClient.clickGuiScreen;
 
 public class Ui extends Module {
     public final BoolSetting moduleFill = addSetting(new BoolSetting("ModuleFill", true));
@@ -20,13 +16,5 @@ public class Ui extends Module {
     public Ui() {
         super("UI", "Management to your click gui", Category.Client);
         //add(GLFW.GLFW_KEY_RIGHT_SHIFT);
-    }
-    
-    @Override
-    public void onEnabled() {
-        if (mc == null || mc.mouse == null) return;
-        
-        mc.setScreen(clickGuiScreen);
-        this.toggle();
     }
 }
