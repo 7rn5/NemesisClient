6f0a76faf36f3e773dfdd7f3fe89543fa1b0ad83
7rn5 <zyagaimo7103@gmail.com>
2025-08-15 14:58:33 +0900

    update src/main/java/nemesis/impl/gui/widget/Widget.java

---
+++ src/main/java/nemesis/impl/gui/widget/Widget.java
@@ -0,0 +1,16 @@
+package nemesis.impl.gui.widget;
+
+import nemesis.settings.Setting;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+
+public interface Widget<T extends Setting<?>> {
+    int HEIGHT = 12;
+    int WIDTH = 50;
+    int PADDING = 2;
+    int SLIDER_HEIGHT = 6;
+    
+    void render(DrawContext context, TextRenderer textRenderer, T setting, int x, int y, int mouseX, int mouseY);
+    boolean mouseClicked(T setting, double mouseX, double mouseY, int button);
+    void mouseDragged(T setting, double mouseX);
+}
