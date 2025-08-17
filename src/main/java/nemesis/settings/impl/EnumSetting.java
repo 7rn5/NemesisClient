5dabddad31828a4a695d101621891e1900a7baa3
7rn5 <zyagaimo7103@gmail.com>
2025-08-18 04:09:43 +0900

    update src/main/java/nemesis/settings/impl/EnumSetting.java

--- src/main/java/nemesis/settings/impl/EnumSetting.java
+++ src/main/java/nemesis/settings/impl/EnumSetting.java
@@ -1,15 +1,16 @@
-package nemesis.settings.impl;
+/*package nemesis.settings.impl;
 
 //import com.google.gson.JsonElement;
 //import com.google.gson.JsonPrimitive;
 import nemesis.settings.Setting;
+import java.util.*;
 
 public class EnumSetting<T extends Enum<T>> extends Setting<T> {
-    private final T[] values;
+    private final List<T> values;
     
     public EnumSetting(String name, T defaultValue) {
         super(name, defaultValue);
-        this.values = defaultValue.getDeclaringClass().getEnumConstants();
+        this.values = Arrays.asList(defaultValue.getDeclaringClass().getEnumConstants());
     }
     
     public void cycle() {
@@ -17,7 +18,7 @@ public class EnumSetting<T extends Enum<T>> extends Setting<T> {
         value = values[index];
     }
     
-    public T[] getValues() {
+    public List<T> getValues() {
         return values;
     }
     
@@ -38,4 +39,36 @@ public class EnumSetting<T extends Enum<T>> extends Setting<T> {
     //public JsonElement toJson() {
     //    return new JsonPrimitive(value.name());
     //}
+}*/
+
+
+package nemesis.settings.impl;
+
+import nemesis.settings.Setting;
+import java.util.Arrays;
+import java.util.List;
+
+public class EnumSetting<T extends Enum<T>> extends Setting<T> {
+    private final List<T> values;
+
+    public EnumSetting(String name, T defaultValue) {
+        super(name, defaultValue);
+        this.values = Arrays.asList(defaultValue.getDeclaringClass().getEnumConstants());
+    }
+
+    public List<T> getValues() {
+        return values;
+    }
+
+    public void next() {
+        int index = values.indexOf(get()) + 1;
+        if (index >= values.size()) index = 0;
+        set(values.get(index));
+    }
+
+    public void previous() {
+        int index = values.indexOf(get()) - 1;
+        if (index < 0) index = values.size() - 1;
+        set(values.get(index));
+    }
 }
