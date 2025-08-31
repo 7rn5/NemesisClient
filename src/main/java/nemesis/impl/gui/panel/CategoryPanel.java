b30253af3d8e2ce21c047476cdc6bdaaa15f7d8c
7rn5 <zyagaimo7103@gmail.com>
2025-08-31 18:31:28 +0900

    update src/main/java/nemesis/impl/gui/panel/CategoryPanel.java

--- src/main/java/nemesis/impl/gui/panel/CategoryPanel.java
+++ src/main/java/nemesis/impl/gui/panel/CategoryPanel.java
@@ -1,5 +1,6 @@
 package nemesis.impl.gui.panel;
 
+import nemesis.impl.gui.widget.Widget;
 import nemesis.impl.module.Module;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
@@ -11,21 +12,10 @@ import static nemesis.NemesisClient.moduleManager;
 
 public class CategoryPanel {
     private final Module.Category category;
+    private boolean expanded = true;
     private final List<ModulePanel> modulePanels = new ArrayList<>();
     private final List<Module> modules;
     private int x, y;
-
-    /*public CategoryPanel(Module.Category category, int x, int y) {
-    *    this.category = category;
-    *    this.x = x;
-    *    this.y = y;
-    *
-    *    int offsetY = 20;
-    *    for (Module module : moduleManager.getByCategory(category)) {
-    *        modulePanels.add(new ModulePanel(module, x + 5, y + offsetY));
-    *        offsetY += 60; // モジュール1個分の高さ仮置き
-    *    }
-    }*/
     
     public CategoryPanel(Module.Category category, List<Module> modules, int x, int y) {
         this.category = category;
@@ -39,20 +29,32 @@ public class CategoryPanel {
             offsetY += 14;
         }
     }
-
+    
     public void render(DrawContext context, TextRenderer textRenderer, int mouseX, int mouseY) {
         // カテゴリ名
         context.drawText(textRenderer, category.getName(), x, y, 0x00FFAA, false);
-
-        for (ModulePanel panel : modulePanels) {
-            panel.render(context, textRenderer, mouseX, mouseY);
+        if (expanded) {
+            for (ModulePanel panel : modulePanels) {
+                panel.render(context, textRenderer, mouseX, mouseY);
+            }
         }
     }
-
+    
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        for (ModulePanel panel : modulePanels) {
-            if (panel.mouseClicked(mouseX, mouseY, button)) return true;
+        if (button == 1 && isHovered(mouseX, mouseY)) {
+            expanded = !expanded;
+            return true;
+        }
+        if (expanded) {
+            for (ModulePanel panel : modulePanels) {
+                if (panel.mouseClicked(mouseX, mouseY, button)) return true;
+            }
         }
         return false;
+    }
+    
+    private boolean isHovered(double mouseX, double mouseY) {
+        return mouseX >= this.x && mouseX <= this.x + Widget.WIDTH &&
+                mouseY >= this.y && mouseY <= this.y + Widget.HEIGHT;
     }
 }
