e827f2680942cb9ee6783e3133884a6178791aff
7rn5 <zyagaimo7103@gmail.com>
2025-09-22 09:15:44 +0900

    Add files via upload

---
+++ src/main/java/nemesis/impl/module/combat/Offhand.java
@@ -0,0 +1,9 @@
+package nemesis.impl.module.combat;
+
+import nemesis.impl.module.Module;
+
+public class Offhand extends Module {
+    public Offhand() {
+        super("Offhand", "Auto swap to offhand", Category.Combat);
+    }
+}
