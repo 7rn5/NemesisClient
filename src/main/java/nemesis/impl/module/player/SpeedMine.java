01b3e72ae1e7902eb49ee7d3e90bd7e70d46f424
7rn5 <zyagaimo7103@gmail.com>
2025-09-22 09:19:57 +0900

    Add files via upload

---
+++ src/main/java/nemesis/impl/module/player/SpeedMine.java
@@ -0,0 +1,9 @@
+package nemesis.impl.module.player;
+
+import nemesis.impl.module.Module;
+
+public class SpeedMine extends Module {
+    public SpeedMine() {
+        super("SpeedMine", "Very fast mine!!", Category.Player);
+    }
+}
