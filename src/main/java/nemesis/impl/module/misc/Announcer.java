3ee3780e80a06760851c43ca1e96975fd94e7d62
7rn5 <zyagaimo7103@gmail.com>
2025-09-22 09:16:51 +0900

    Add files via upload

---
+++ src/main/java/nemesis/impl/module/misc/Announcer.java
@@ -0,0 +1,9 @@
+package nemesis.impl.module.misc;
+
+import nemesis.impl.module.Module;
+
+public class Announcer extends Module {
+    public Announcer() {
+        super("Announcer", "Announce to client/player", Category.Misc);
+    }
+}
